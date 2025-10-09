11fc4974a8c19ae825aca9fa166c5f1b462cf8fb
Nikola <nikolamandan@gmail.com>
2025-10-10 00:48:15 +0200

    Definisane API rute za movies rating i comments

--- routes/api.php
+++ routes/api.php
@@ -3,16 +3,18 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
+Route::apiResource('movies', MoviesController::class);
+
+Route::get('movies/{movie}/ratings', [RatingsController::class, 'index']);
+Route::post('movies/{movie}/ratings', [RatingsController::class, 'store']);
+Route::put('ratings/{id}', [RatingsController::class, 'update']);
+Route::delete('ratings/{id}', [RatingsController::class, 'destroy']);
+
+Route::get('movies/{movie}/comments', [CommentsController::class, 'index']);
+Route::post('movies/{movie}/comments', [CommentsController::class, 'store']);
+Route::put('comments/{id}', [CommentsController::class, 'update']);
+Route::delete('comments/{id}', [CommentsController::class, 'destroy']);
+
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
