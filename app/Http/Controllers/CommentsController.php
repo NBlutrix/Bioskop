c94586ed38b50efdd77cd4e46488abede5219232
Nikola <nikolamandan@gmail.com>
2025-10-10 00:44:34 +0200

    Kreiranje CommentsController

---
+++ app/Http/Controllers/CommentsController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class CommentsController extends Controller
+{
+    public function index($movieId) {
+    return Comment::where('movie_id', $movieId)->with('user')->get();
+}
+
+public function store(Request $request, $movieId) {
+    $comment = Comment::create([
+        'user_id' => $request->user()->id,
+        'movie_id' => $movieId,
+        'content' => $request->content
+    ]);
+    return response()->json($comment, 201);
+}
+
+public function update(Request $request, $id) {
+    $comment = Comment::findOrFail($id);
+    $comment->update($request->only('content'));
+    return response()->json($comment);
+}
+
+public function destroy($id) {
+    Comment::destroy($id);
+    return response()->json(null, 204);
+}
+}
